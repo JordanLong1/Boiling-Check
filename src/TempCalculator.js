aadf8b32677e0d1646b906dd9f19fb0adf6efa89
Jordan <jordanleelong5@gmail.com>
2020-11-12 13:29:16 -0800

    added helper methods and moved state up

--- src/TempCalculator.js
+++ src/TempCalculator.js
@@ -1,28 +1,69 @@
 import React from 'react'; 
 import BoilingCheck from './BoilingCheck'
+import TempInput from './TempInput'
+
+
+
+
 class TempCalculator extends React.Component {
 
-    state = {temperture: ''}
+    constructor(props) {
+        super(props) 
+        this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+        this.handleFarenheitChange = this.handleFarenheitChange.bind(this);
+        this.state = {temperature: '', scale: 'c'}
+    }
 
-    handleChange = event => {
-        this.setState({
-            temperture: event.target.value
-        })
+  
+    handleCelsiusChange(temperature) {
+        this.setState({scale: 'c', temperature: temperature});
+      }
+    
+      handleFarenheitChange (temperature) {
+        this.setState({scale: 'f', temperature: temperature});
+      }
+
+      toCelsius(farenheit) {
+        return (farenheit - 32) * 5 /9;
+    }
+    
+     toFarenheit(celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+    
+     tryConvert(temperature, convert) {
+        const input = parseFloat(temperature); 
+        if (Number.isNaN(input)) {
+            return '';
+        }
+        const output = convert(input); 
+        const rounded = Math.round(output * 1000) / 1000
+        return rounded.toString();
     }
 
 
     render() {
-        const temp = this.state.temperture
-        return (
-            <fieldset>
-                <label>Enter a temperatue below:</label>
-                <input 
-                value={temp} 
-                onChange={this.handleChange}
-                />
-        <BoilingCheck degrees={parseFloat(temp)}/>
-            </fieldset>
-        )
+        const scale = this.state.scale; 
+        const temperature = this.state.temperature; 
+        const celsius = scale === 'f' ? this.tryConvert(temperature, this.toCelsius) : temperature
+        const farenheit = scale === 'c' ? this.tryConvert(temperature, this.toFarenheit) : temperature
+       return (
+           <div>
+               <TempInput 
+               scale='c'
+               temperature={celsius}
+               onTemperatureChange={ this.handleCelsiusChange}
+               />
+               <TempInput 
+               scale='f'
+               temperature={farenheit}
+               onTemperatureChange={this.handleFarenheitChange}
+               />
+               <BoilingCheck 
+                degrees={parseFloat(celsius)}
+               />
+           </div>
+       )
     };
 };
 
